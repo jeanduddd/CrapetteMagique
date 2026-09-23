5a5389576e21d709dabc99d0aae717922eda1adf
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-23 22:26:03 +0200

    feat : add a fake gameState to test frontend more easily

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,60 @@ import { useState } from "react"
 import { io, Socket } from "socket.io-client"
 import MenuScreen from "./screens/menu/MenuScreen"
 import WaitingScreen from "./screens/waiting/WaitingScreen"
-import type { GameState } from "@shared/IPlayCard"
+import type { CardData, GameState, PileData } from "@shared/IPlayCard"
 import FullRoomScreen from "./screens/full/FullRoomScreen"
 import WonGameScreen from "./screens/won/WonGameScreen"
 import LostGameScreen from "./screens/lost/LostGameScreen"
 import DefaultWinGameScreen from "./screens/defaultWin/DefaultWinGameScreen"
 import Game from "./screens/game/Game"
 
+
+function instanciateCard(value: number, symbol: string):CardData{
+    const card: CardData = {
+        value: value,
+        symbol: symbol
+    }
+    return card
+}
+
+function instanciatePileData(nb: number, cards?: CardData[]):PileData{
+    const pile: PileData = {
+        cardNumber: nb,
+        cards: cards
+    }
+    return pile
+}
+
 export default function App(){
-    const [view, setView] = useState<'MENU' | 'WAITING' | 'GAME' |'FULL' | 'WON' | 'LOST' | 'DEFAULT'>('MENU')
+    const [view, setView] = useState<'MENU' | 'WAITING' | 'GAME' |'FULL' | 'WON' | 'LOST' | 'DEFAULT'>('GAME')
 
     const [name, setName] = useState('')
 
     const [socket, setSocket] = useState<Socket | null>(null);
 
-    const [gameState, setGameState] = useState<GameState | null>(null);
+    const draw: PileData = instanciatePileData(-1,[])
+    const enemyDraw: PileData = instanciatePileData(-1,[])
+    const bin: PileData = instanciatePileData(0,[])
+    const enemyBin: PileData = instanciatePileData(0,[])
+    const crapette: PileData = instanciatePileData(1,[instanciateCard(3,"heart")])
+    const enemyCrapette: PileData = instanciatePileData(1,[instanciateCard(9,"club")])
+    const aces: PileData[] = [instanciatePileData(0), instanciatePileData(1,[instanciateCard(1,"spade")]), instanciatePileData(0), instanciatePileData(0), instanciatePileData(1,[instanciateCard(4,"diamond")]), instanciatePileData(0), instanciatePileData(0), instanciatePileData(0)]
+    const board: PileData[] = [instanciatePileData(3,[instanciateCard(13,"heart"),instanciateCard(12,"club"),instanciateCard(11,"diamond")]),instanciatePileData(0),instanciatePileData(1,[instanciateCard(3,"heart")]),instanciatePileData(0),instanciatePileData(2,[instanciateCard(3,"spade"), instanciateCard(2,"heart")]),instanciatePileData(0),instanciatePileData(1,[instanciateCard(11,"diamond")]),instanciatePileData(1,[instanciateCard(4,"club")])]
+
+    const newGameState: GameState = {
+        myTurn: true,
+        crapette: crapette,
+        enemyCrapette: enemyCrapette,
+        bin: bin,
+        enemyBin: enemyBin,
+        draw: draw,
+        enemyDraw: enemyDraw,
+        aces:aces,
+        board:board
+    }
+
+    const [gameState, setGameState] = useState<GameState | null>(newGameState);// remettre à null...
+
 
     const connectToServer = (playerID: string | null, playerName: string) => {
         const connection = io('http://localhost:3001', {
@@ -65,7 +104,7 @@ export default function App(){
         setSocket(null)
         setView('MENU')
     }
-
+   
     return (
     <>
         {view === 'MENU' && <MenuScreen name={name} handleNameChange={setName} handleClick={handleClicPlay}></MenuScreen>}
