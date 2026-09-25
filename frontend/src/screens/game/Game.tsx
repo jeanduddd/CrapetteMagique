32a9df5a8ee20162f860644908d808fbf8e184a2
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-25 16:26:30 +0200

    feat : spread the props to reveal the draw

--- frontend/src/screens/game/Game.tsx
+++ frontend/src/screens/game/Game.tsx
@@ -5,7 +5,7 @@ import styles from "./style/GameScreenStyle"
 import Board from "./components/Board"
 import GameStyles from "./style/GameScreenStyle"
 
-const Game = ({gameState, setOrigin, setDestination, origin} : GameProps) => {
+const Game = ({gameState, setOrigin, setDestination, origin, revealDraw} : GameProps) => {
 
     //haut, bas, cartes à gauche, as, cartes à droite
 
@@ -19,13 +19,13 @@ const Game = ({gameState, setOrigin, setDestination, origin} : GameProps) => {
     return (
         <div style={styles.screen}>
             <div style={{height: handSize, width: '100vw', ...GameStyles.border}}>
-                <EnemyHand setOrigin={setOrigin} setDestination={setDestination} crapette={gameState.enemyCrapette} bin={gameState.enemyBin} draw={gameState.enemyDraw}></EnemyHand>
+                <EnemyHand revealDraw={revealDraw} setOrigin={setOrigin} setDestination={setDestination} crapette={gameState.enemyCrapette} bin={gameState.enemyBin} draw={gameState.enemyDraw}></EnemyHand>
             </div>
             <div style={{height: boardSize, width: '100vw', ...GameStyles.border}}>
                 <Board origin={origin} setOrigin={setOrigin} setDestination={setDestination} aces={gameState.aces} boardPiles={gameState.board}></Board>
             </div>
             <div style={{height: handSize, width: '100vw', ...GameStyles.border}}>
-                <MyHand setOrigin={setOrigin} setDestination={setDestination} crapette={gameState.crapette} bin={gameState.bin} draw={gameState.draw}></MyHand> 
+                <MyHand revealDraw={revealDraw} setOrigin={setOrigin} setDestination={setDestination} crapette={gameState.crapette} bin={gameState.bin} draw={gameState.draw}></MyHand> 
             </div>
         </div>
     )
--- frontend/src/screens/game/properties/gameProps.ts
+++ frontend/src/screens/game/properties/gameProps.ts
@@ -5,4 +5,5 @@ export type GameProps = {
     setOrigin: (arg0: Location) => void
     setDestination: (arg0: Location) => void
     origin: Location | null
+    revealDraw: () => void
 }
