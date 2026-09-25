dae9d0ff955fa26db1a8bc993d20c5d79eda633b
jeanduddd <jeanduddd@gmail.com>
2026-09-25 20:51:42 +0200

    fix...

--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -15,9 +15,10 @@ const server = createServer(app);
 const distPath = path.join(__dirname, 'dist')
 app.use(express.static(distPath))
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'))
 })
+
 const io = new Server(server, {
   cors: { origin: "*" },
 });
