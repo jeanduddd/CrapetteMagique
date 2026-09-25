1c5d9428f528aefc346b5212fe3df43b14084e00
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-25 18:14:47 +0200

    fix : wrong pile names were used...

--- frontend/src/screens/game/components/EnemyHand.tsx
+++ frontend/src/screens/game/components/EnemyHand.tsx
@@ -12,8 +12,8 @@ const EnemyHand = ({crapette, bin, draw, setDestination}: HandsProps) => {
     return (
         <div style={{...handStyles.hand}}>
             {crapette.cardNumber === 0 ? <img draggable={false} style={cardStyles.card} src={'forbidden_spot.png'}></img> : <img draggable={false} onDrop={() => dragEnd("CRAPETTE")} onDragOver={(e) => {e.preventDefault()}} style={cardStyles.card} src={`${crapette.cards?.[0].symbol}_${crapette.cards?.[0].value}.png`}></img>}
-            {bin.cardNumber === 0 ? <img draggable={false} style={cardStyles.card} src={'trash_spot.png'}></img> : <img draggable={false} onDrop={() => dragEnd("BIN")} onDragOver={(e) => {e.preventDefault()}} style={cardStyles.card} src={`${bin.cards?.[0].symbol}_${crapette.cards?.[0].value}.png`}></img>}
-            {draw.cardNumber === -1 ? <img draggable={false} style={cardStyles.card} src={'card_back.png'}></img> : draw.cardNumber === 0 ? <img draggable={false} style={cardStyles.card} src={'empty_spot.png'}></img> : <img draggable={false} style={cardStyles.card} src={`${bin.cards?.[0].symbol}_${crapette.cards?.[0].value}.png`}></img>}
+            {bin.cardNumber === 0 ? <img draggable={false} style={cardStyles.card} src={'trash_spot.png'}></img> : <img draggable={false} onDrop={() => dragEnd("BIN")} onDragOver={(e) => {e.preventDefault()}} style={cardStyles.card} src={`${bin.cards?.[0].symbol}_${bin.cards?.[0].value}.png`}></img>}
+            {draw.cardNumber === -1 ? <img draggable={false} style={cardStyles.card} src={'card_back.png'}></img> : draw.cardNumber === 0 ? <img draggable={false} style={cardStyles.card} src={'empty_spot.png'}></img> : <img draggable={false} style={cardStyles.card} src={`${draw.cards?.[0].symbol}_${draw.cards?.[0].value}.png`}></img>}
         </div>
     )
 }
