5d974c0f113531e933563a4eee77fde83af9978a
jeanduddd <jeanduddd@gmail.com>
2026-09-25 22:37:01 +0200

    feat : adapt ace gap

--- frontend/src/screens/game/style/aceLineStyle.ts
+++ frontend/src/screens/game/style/aceLineStyle.ts
@@ -5,7 +5,7 @@ const aceLineStyle = {
         height: (window.innerHeight/6),
         alignItems: 'center',
         justifyContent: 'center',
-        gap: (window.innerWidth/6) / 7,
+        gap: (window.innerWidth/4) / 8,
         margin: 0,
         padding: 0
     }
